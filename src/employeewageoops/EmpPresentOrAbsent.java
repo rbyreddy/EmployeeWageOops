144f1df72e3f4cd9013808556f39b8b03f9550ec
Rakesh <byreddyrakesh41@gmail.com>
2022-10-31 21:14:53 +0530

    Added: Code for the Employee is Present or Absent

--- src/employeewageoops/EmpPresentOrAbsent.java
+++ src/employeewageoops/EmpPresentOrAbsent.java
@@ -1,16 +1,33 @@
 package employeewageoops;
-// To check whether Employee is Present or Absent
 
 public class EmpPresentOrAbsent {
-        public static void main(String args[]) {
-            int full_time = 1;
+    //This program is to display the welcome message to Employee wage computation program.
+//Use Case-1 is to check whether the Employee is present or not.
 
-            double check = Math.floor(Math.random() * 10) % 2;
+        // Default Constructor
+        public EmpPresentOrAbsent() {
+            System.out.println("Welcome to Employee Wage Computation Program..!");
+        }
 
-            if (check == full_time)
+        public static void main(String[] args) {
+
+            //Initializing the constructor by creating the object of the class.
+            EmpPresentOrAbsent employee = new EmpPresentOrAbsent();
+            System.out.println();
+
+            //Declaring the variables.
+            double isPresent = 1;
+            double randomCheck = (double)Math.floor((Math.random()*10)%2);
+
+            //Checking if the Employee is present!!!
+            if (randomCheck == isPresent)
+            {
                 System.out.println("Employee is present");
+            }
             else
-                System.out.println("Employee is Absent");
+            {
+                System.out.println("Employee is absent");
+            }
 
         }
     }
