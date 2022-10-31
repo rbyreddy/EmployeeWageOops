b7aabeedbb5f7b83546085e20cb1f4eb21ccdb41
Rakesh <byreddyrakesh41@gmail.com>
2022-10-31 21:30:50 +0530

    Added: Code for the DailyWage Calculation

--- src/employeewageoops/EmpWageComputation.java
+++ src/employeewageoops/EmpWageComputation.java
@@ -1,34 +1,42 @@
 package employeewageoops;
 
-public class EmpPresentOrAbsent {
+public class EmpWageComputation {
     //This program is to display the welcome message to Employee wage computation program.
 //Use Case-1 is to check whether the Employee is present or not.
+// Use Case-2 is to calculate the daily Employee wage.
 
         // Default Constructor
-        public EmpPresentOrAbsent() {
+        public EmpWageComputation() {
             System.out.println("Welcome to Employee Wage Computation Program..!");
         }
 
         public static void main(String[] args) {
 
             //Initializing the constructor by creating the object of the class.
-            EmpPresentOrAbsent employee = new EmpPresentOrAbsent();
+            EmpWageComputation employee = new EmpWageComputation();
             System.out.println();
 
             //Declaring the variables.
             double isPresent = 1;
             double randomCheck = (double)Math.floor((Math.random()*10)%2);
+            int fullDayHour = 8;
+            int wagePerHour = 20;
+            int salary = 0;
 
-            //Checking if the Employee is present!!!
+            //Checking if the Employee is present and then calculating his/her daily wage.
             if (randomCheck == isPresent)
             {
                 System.out.println("Employee is present");
+                salary = fullDayHour * wagePerHour;
+                System.out.println("Salary is: $"+salary);
             }
             else
             {
                 System.out.println("Employee is absent");
+                System.out.println("Salary is: $"+salary);
             }
 
         }
     }
+
 
